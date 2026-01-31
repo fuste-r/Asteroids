3dce839ea10ab823295d0a3947269f062f1874a8
fuste-r <cjbookheimer@gmail.com>
2026-01-30 21:59:11 -0500

    add circleshape.py

--- main.py
+++ main.py
@@ -2,21 +2,26 @@ import pygame
 from constants import *
 from logger import log_state
 def main():
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt =0
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     # begin game loop
-    while 1==1:
+    while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         log_state()
         screen.fill("black")
         pygame.display.flip()
-            
-
+        clock.tick(60)    
+        dt = (clock.tick(60))
+        print(dt)
 if __name__ == "__main__":
     main()
